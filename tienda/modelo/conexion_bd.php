27e39c37b7b0d9b02b8f2e952e1af09c7c43800c
Daniel Bueno Navarro <danielbuenonavarro@gmail.com>
2024-10-07 20:59:11 +0200

    Cambios

---
+++ tienda/modelo/conexion_bd.php
@@ -0,0 +1,10 @@
+<?php
+require_once("variables_entorno.php");
+$base_datos = new mysqli($host, $usuario_bd, $pass_bd, $nombre_bd);
+
+// Verificar conexión
+if ($base_datos->connect_error) {
+    die("Error en la conexión: " . $mysqli->connect_error);
+} else {
+    echo "Hola estoy conectado";
+}
